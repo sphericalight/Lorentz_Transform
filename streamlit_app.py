8ab082ee1f9f9531054c3d58bcb8a5199f6d2446
sphericalight <paulhinmilton@gmail.com>
2025-03-23 14:19:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 # u is velocity that define the event or velocity of second as the fraction to speed of light c (relative to stationary frame)
 
 # User input
-ct = st.number_input("Time (t)", value=1.0)
-v = st.slider("Velocity (v/c)", 0.0, 0.99, 0.5)
-u = st.slider("Velocity (u/c)", 0.0, 0.99, 0.5)
+ct = st.number_input("Time (t)", value=5.0)
+v = st.slider("Velocity (v/c)", 0.0, 0.99, 0.6)
+u = st.slider("Velocity (u/c)", 0.0, 0.99, 0.8)
 
 #col1, col2 = st.columns([1, 5])  # Narrow column on the left
 
